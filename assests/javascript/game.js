27ed2ea7a3955327b56874b0c0394cfb50253bf4
Broc <broc.davis@yahoo.com>
2019-09-22 18:55:18 -0500

    implemented function for which letter is pressed to play the game and will recongnize letter press and will add to wins if word is guessed correctly

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,11 +1,12 @@
-var wordBank = ["boat", "water", "ski", "wakebord", "float", "swim", "tube","anchor","waves",""];
+var wordBank = ["boat", "water", "ski", "wakebord", "float", "swim", "tube", "anchor", "waves", ""];
 var wrongLetters = [];
-var correctWord;
+var userGuess = [];
+var letterGuess;
 
 var winCount = 0;
 var lostCount = 0;
 var maxGuess = 7;
-var userGuess;
+var gameRun = true;
 
 //call at beginning and end of code win or lose to rest game and change word
 function reset() {
@@ -15,17 +16,40 @@ function reset() {
     correctWord = document.getElementById("correct-word");
     userGuess = document.getElementById("letters-guessed");
 }
-
+//calling function to reset game upon load
 reset();
 
-document.onkeyup = function () {
-
-    userGuess = event.key;
-    if (userGuess = "a")
-
-        if (!wrongLetters.length === maxGuess) {
-
-
-        }
+//function checking for button press if its a letter only
+var isCharacter = function (ch) {
+    return /^[A-Z]$/i.test(ch);
 }
+
+//once button is pressed pass in game logic
+document.onkeypress = function (event) {
+
+    //making any guess from input change to uppercase
+    if (isCharacter(event.key) && gameRun) {
+        letterPressed(event.key.toUpperCase());
+        console.log(event.key)
+    }
+}
+
+function letterPressed(letter){
+    var letterPressed = false;
+
+    for(var i = 0; i < userGuess.length; i++){
+        if(letter === userGuess[i]){
+            userGuess[i] = letter;
+            letterPressed = true;
+        }
+
+        if(userGuess.join("")=== letterGuess){
+            wins++;
+            gameRun = false;
+        }
+    }
+}
+
+
+
 
